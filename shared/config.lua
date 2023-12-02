dfc9b2f10c220eeceb140bc79ebb90d361bb3397
Domi <106663559+easydomii@users.noreply.github.com>
2023-12-02 04:39:19 +0100

    Update config.lua

--- shared/config.lua
+++ shared/config.lua
@@ -1,5 +1,5 @@
 Config = {}
 
-Config.Framework = 'qb' -- qb or esx
+Config.Framework = 'esx' -- qb or esx
 
 Config.UseSmokeMonsterScreenEffect = true -- Set to true to have a screen effect applied while in the smoke monster effect
